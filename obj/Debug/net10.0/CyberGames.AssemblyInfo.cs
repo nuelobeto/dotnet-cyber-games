003d3cca377787117260bedc8420ff8995cc9f26
nuelobeto <nuelobeto@gmail.com>
2026-01-29 09:07:14 +0000

    Update static web assets and add focusElement function to site.js
    - Updated staticwebassets.build.json.cache and staticwebassets.development.json to reflect changes in asset paths and hashes.
    - Added new JavaScript function `focusElement` in site.js to facilitate focusing on a specified DOM element.

--- obj/Debug/net10.0/CyberGames.AssemblyInfo.cs
+++ obj/Debug/net10.0/CyberGames.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486127d36bd5dda731baa36325c25475e947d4e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3b8f4b21b7147186aaed723660d30324deb920")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/site.js
@@ -0,0 +1,5 @@
+window.focusElement = (element) => {
+	if (element) {
+		element.focus();
+	}
+};
